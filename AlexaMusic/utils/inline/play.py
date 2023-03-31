c38159ddf8de9969a8dac660e68db597eec3b31e
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-03-31 01:09:36 -0700

    Update play.py

--- AlexaMusic/utils/inline/play.py
+++ AlexaMusic/utils/inline/play.py
@@ -30,7 +30,8 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-    ]
+    ],
+   ]
     return buttons
 
 
@@ -45,7 +46,8 @@ def telegram_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-    ]
+    ],
+  ]
     return buttons
 
 
@@ -62,7 +64,8 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-    ]
+    ],
+  ]
     return buttons
 
 
